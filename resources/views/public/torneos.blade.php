f61cd1e948bb76429d4739ed6d6d65e1f4a91c3c
Isaias123Bv <114431930+Isaias123Bv@users.noreply.github.com>
2024-08-15 12:19:56 -0600

    Update torneos.blade.php

--- resources/views/public/torneos.blade.php
+++ resources/views/public/torneos.blade.php
@@ -152,9 +152,6 @@
                 @foreach ($torneos as $torneo)
                     <div class="col-md-3 mb-4">
                         <div class="card h-100">
-                            @if ($torneo['logo'])
-                        
-                            @else
                                 <div class="card-img-top d-flex align-items-center justify-content-center p-3 bg-light mx-auto" style="height: 75px; width: 75px;">
                                     <span class="text-muted">N/A</span>
                                 </div>
