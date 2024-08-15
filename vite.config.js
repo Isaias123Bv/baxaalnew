912f4b9af3db3b60107c88244b545c7c1d2e5ba6
Isaias123Bv <114431930+Isaias123Bv@users.noreply.github.com>
2024-08-14 22:52:29 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/js/app.js', 'resources/css/app.css'],
-      refresh: true,
+      input: ['resources/js/app.js', 'resources/css/app.css'], // Rutas de entrada de tus archivos
+      refresh: true, // Habilita la actualización en caliente durante el desarrollo
     }),
   ],
 });
